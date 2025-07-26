a4c29b76c974115d79f789bf9caff0053d679067
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-26 22:58:14 +0300

    Update src/lib/utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,6 @@ export function formatDate(date: Date): string {
   return new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
     month: 'long',
-    day: 'numeric'
+    day: '2-digit',
   }).format(date)
 }
