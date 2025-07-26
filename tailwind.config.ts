af87d3ca504200b848feb7447e4deda99870e921
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-26 23:13:07 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
-import { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
